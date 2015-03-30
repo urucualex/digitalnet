3c88abad05f0ece11de2db686d5a5a4dfb976058
Alex Urucu <quigonjin2@gmail.com>
2015-03-30 16:51:51 +0300

    Add menu

---
+++ application/views/menu.php
@@ -0,0 +1,29 @@
+<header class="navbar navbar-static-top bs-docs-nav navbar-inverse" id="top" role="banner">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="/" class="navbar-brand">DigitalNet</a>
+        </div>
+        <nav class="collapse navbar-collapse bs-navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="/clients">Statii</a>
+                </li>
+                <li>
+                    <a href="/campaigns">Campanii</a>
+                </li>
+                <li>
+                    <a href="/log">Log</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/user">User Name</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>     
